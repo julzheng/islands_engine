c0c752fdb5a627479ec97f61b501976afc9abb13
Julian Zheng <jul@julianzheng.com>
2020-02-28 17:00:19 +0700

    page 81 - Position Islands

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -12,6 +12,28 @@ defmodule IslandsEngine.Game do
     {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
   end
   
+
+  def add_player(game, name) when is_binary(name), do:
+  GenServer.call(game, {:add_player, name})
+
+  def handle_call({:add_player, name}, _from, state_data) do
+    with {:ok, rules} <- Rules.check(state_data.rules, :add_player)
+      do
+      state_data
+      |> update_player2_name(name)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+      else
+        :error ->  {:reply, :error, state_data}
+    end
+  end
+
+  defp update_player2_name(state_data, name), do:
+  put_in(state_data.player2.name, name)
+
+  defp update_rules(state_data, rules), do: %{state_data | rules: rules}
+
+  defp reply_success(state_data, reply), do: {:reply, reply, state_data}
   
   # def handle_info(:first, state) do
   #   IO.puts "This message has been handled by handle_info/2, matching on :first."
