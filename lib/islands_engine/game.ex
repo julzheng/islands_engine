5a094480ea17fb6c9e73ddd7b28eb74e85eb2abd
Julian Zheng <jul@julianzheng.com>
2020-03-05 16:52:33 +0700

    page 123

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -8,11 +8,9 @@ defmodule IslandsEngine.Game do
   def start_link(name) when is_binary(name), do:
   GenServer.start_link(__MODULE__, name, name: via_tuple(name))
 
-  @timeout 60 * 60 * 24 * 1000
   def init(name) do
-    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
-    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
-    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}, @timeout}
+    send(self(), {:set_state, name})
+    {:ok, fresh_state(name)}
   end
   
 
@@ -109,6 +107,18 @@ defmodule IslandsEngine.Game do
   def handle_info(:timeout, state_data) do
     {:stop, {:shutdown, :timeout}, state_data}
   end
+
+
+  @timeout 60 * 60 * 24 * 1000
+  def handle_info({:set_state, name}, _state_data) do
+    state_data =
+      case :ets.lookup(:game_state, name) do
+        [] -> fresh_state(name)
+        [{_key, state}] -> state
+      end
+    :ets.insert(:game_state, {name, state_data})
+    {:noreply, state_data, @timeout}
+  end
   
   def via_tuple(name), do: {:via, Registry, {Registry.Game, name}}
   
@@ -118,6 +128,7 @@ defmodule IslandsEngine.Game do
   defp update_rules(state_data, rules), do: %{state_data | rules: rules}
 
   defp reply_success(state_data, reply) do
+    :ets.insert(:game_state, {state_data.player1.name, state_data})
     {:reply, reply, state_data, @timeout}
   end
 
@@ -133,6 +144,11 @@ defmodule IslandsEngine.Game do
     update_in(state_data[player_key].guesses, fn guesses ->
       Guesses.add(guesses, hit_or_miss, coordinate)
     end)
+  end
+
+  defp fresh_state(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}      %{player1: player1, player2: player2, rules %Rules{}}
   end
   
   # def handle_info(:first, state) do
